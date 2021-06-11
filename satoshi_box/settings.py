11a3a71a36403dc019fecf0d601425ceae72a1d0
cnohall <christopher.nohall@gmail.com>
2021-06-11 23:00:58 +0900

    add static_root

--- satoshi_box/settings.py
+++ satoshi_box/settings.py
@@ -19,7 +19,7 @@ load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
